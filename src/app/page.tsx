0c8e62a44bf7b98ad9480d249942ca44bc8df415
Lee-Seungje <moondgod@gmail.com>
2023-12-07 01:05:54 +0900

    Add types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,23 @@
 'use client';
 
-import { useEffect } from 'react';
-
 import axios from 'axios';
 
 export default function Home() {
-  const test = async () => {
+  const postBirthday = async () => {
     const res = await axios.post('/api/birthday/create', {
       name: '이동욱',
       birthday: '2006/12/07',
     });
-    console.log(res);
   };
 
-  useEffect(() => {
-    test();
-  }, []);
-  return <div>test</div>;
+  const getList = async () => {
+    const res = await axios.get('/api/birthday/list');
+  };
+
+  return (
+    <div>
+      <button onClick={postBirthday}>postBirthday</button>
+      <button onClick={getList}>getList</button>
+    </div>
+  );
 }
---
+++ src/types/birthdayResponse.ts
@@ -0,0 +1,7 @@
+export interface BirthdayResponse {
+  id: number;
+  name: string;
+  birthday: Date;
+  createdAt: Date;
+  updatedAt: Date;
+}
