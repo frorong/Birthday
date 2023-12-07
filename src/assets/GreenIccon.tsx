a2821134544023ff265619331ed0b220e85612bd
Lee-Seungje <moondgod@gmail.com>
2023-12-07 22:40:19 +0900

    Add icons

---
+++ src/assets/GreenIccon.tsx
@@ -0,0 +1,18 @@
+const GreenIcon = () => (
+  <svg
+    width='38.375rem'
+    height='14.875rem'
+    viewBox='0 0 614 238'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
+  >
+    <path
+      fillRule='evenodd'
+      clipRule='evenodd'
+      d='M611.548 238H0.611084C82.2292 102.995 332.271 0 477.5 0C622.215 0 617.631 102.267 611.613 236.567C611.591 237.044 611.57 237.522 611.548 238Z'
+      fill='#60EA8B'
+    />
+  </svg>
+);
+
+export default GreenIcon;
---
+++ src/assets/YellowIcon.tsx
@@ -0,0 +1,18 @@
+const YellowIcon = () => (
+  <svg
+    width='17.875rem'
+    height='33.625rem'
+    viewBox='0 0 286 538'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
+  >
+    <path
+      fillRule='evenodd'
+      clipRule='evenodd'
+      d='M286 537.663C154.594 497.548 0 366.737 0 251.5C0 113.429 105.929 0 244 0C258.313 0 272.345 1.20277 286 3.51276V537.663Z'
+      fill='#FFFD38'
+    />
+  </svg>
+);
+
+export default YellowIcon;
