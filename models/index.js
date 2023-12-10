01a528258b0f8f8970cbe695ef6f93f7b2988f57
Lee-Seungje <moondgod@gmail.com>
2023-12-10 20:56:50 +0900

    Delete useless code

--- models/index.js
+++ models/index.js
@@ -1,9 +1,6 @@
 'use strict';
 
-const fs = require('fs');
-const path = require('path');
 const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
 
 const config = require('../config/config.json');
 
