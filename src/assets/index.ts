1eee6f4fe91113a2736606c40b443363205ea1ce
Lee-Seungje <moondgod@gmail.com>
2023-12-10 15:03:28 +0900

    Add notFound

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,4 +3,5 @@ export { default as YellowIcon } from './YellowIcon';
 export { default as PupleIcon } from './PupleIcon';
 export { default as Frame } from './Frame';
 export { default as BirthdayFrame } from './BirthdayFrame';
+export { default as NotFound } from './NotFound';
 export { default as VectorIcon } from './VectorIcon';
