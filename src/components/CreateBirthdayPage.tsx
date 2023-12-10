26a5d73ab4bf4221478d32fbcabccb371beb3650
Lee-Seungje <moondgod@gmail.com>
2023-12-10 16:37:09 +0900

    Update style

--- src/components/CreateBirthdayPage.tsx
+++ src/components/CreateBirthdayPage.tsx
@@ -8,40 +8,70 @@ import { Dayjs } from 'dayjs';
 import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { MobileDatePicker } from '@mui/x-date-pickers';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const CreateBirthdayPage = () => {
   const [birthday, setBirthday] = useState<Dayjs | null>(null);
+  const [name, setName] = useState<string>('');
 
   const { mutate: birthdayMutate } = usePostBirthday();
 
+  useEffect(() => {
+    const newDate = birthday?.format();
+    if (newDate) console.log(new Date(newDate));
+  }, [birthday]);
+
   return (
-    <Dialog>
-      <FrameContainer>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DemoContainer components={['DatePicker']}>
-            <DatePicker
-              value={birthday}
-              onChange={(newValue) => setBirthday(newValue)}
-            />
-          </DemoContainer>
-        </LocalizationProvider>
-      </FrameContainer>
-      <ButtonWrapper>
-        <SubmitButton>생성</SubmitButton>
-        <SubmitButton>취소</SubmitButton>
-      </ButtonWrapper>
-    </Dialog>
+    <>
+      <Header>
+        <Title>생일 생성하기!</Title>
+      </Header>
+      <Container>
+        <FrameContainer>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DemoContainer components={['DatePicker']}>
+              <MobileDatePicker
+                value={birthday}
+                onChange={(newValue) => setBirthday(newValue)}
+              />
+            </DemoContainer>
+          </LocalizationProvider>
+          <Input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            maxLength={5}
+            placeholder='생일자 이름'
+          />
+        </FrameContainer>
+        <ButtonWrapper>
+          <SubmitButton>생성</SubmitButton>
+          <SubmitButton>취소</SubmitButton>
+        </ButtonWrapper>
+      </Container>
+    </>
   );
 };
 
 export default CreateBirthdayPage;
 
-const Dialog = styled.div`
-  width: 50rem;
-  height: 40rem;
+const Header = styled.div`
+  width: 86.875rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 3.875rem 0 3.875rem;
+`;
+
+const Title = styled.h1`
+  color: #fff;
+  font-size: 2.5rem;
+  font-weight: 600;
+`;
+
+const Container = styled.div`
+  width: 30rem;
   border-radius: 1.25rem;
   background: #fff;
   display: flex;
@@ -53,18 +83,22 @@ const Dialog = styled.div`
 `;
 
 const FrameContainer = styled.div`
-  width: 45rem;
-  height: 29.375rem;
+  width: 25rem;
   border-radius: 0.625rem;
-  border: 0.1875rem solid #000;
+  border: 0.0875rem solid #000;
   background: #e2e7f2;
   margin-top: 2.25rem;
+  padding: 0.625rem 1.875rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1.25rem;
+  align-items: center;
 `;
 
 const SubmitButton = styled.button`
-  width: 17.5rem;
-  height: 6.8125rem;
-  border-radius: 2.5rem;
+  width: 10.5rem;
+  height: 3.8125rem;
+  border-radius: 0.5rem;
   border: 0.125rem solid #000;
   background: #ffeb82;
 
@@ -73,23 +107,30 @@ const SubmitButton = styled.button`
   align-items: center;
 
   color: #000;
-  font-size: 2.5rem;
+  font-size: 1.5rem;
   font-weight: 500;
   margin-bottom: 2.25rem;
 `;
 
-const ButtonWrapper = styled.form`
+const ButtonWrapper = styled.div`
   display: flex;
   gap: 2.625rem;
 `;
 
 const Input = styled.input`
-  width: 18.75rem;
-  height: 2.5rem;
-  font-size: 1.25rem;
+  width: 200px;
+  height: 56px;
+  font-size: 20px;
   font-weight: 600;
-  border: none;
   outline: none;
+  background-color: #e2e7f2;
+  border-radius: 3px;
+  border: 1px solid gray;
+  margin-bottom: 10px;
+
+  :hover {
+    border: 1px solid black;
+  }
 
   ::placeholder {
     color: #ababab;
