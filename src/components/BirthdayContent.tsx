b079391fc6265756b4f71c7af68c8373d2205d59
Lee-Seungje <moondgod@gmail.com>
2023-12-10 12:46:17 +0900

    Update text

--- src/components/BirthdayContent.tsx
+++ src/components/BirthdayContent.tsx
@@ -3,6 +3,7 @@
 import styled from '@emotion/styled';
 
 import { BirthdayFrame } from '@/assets';
+import { formatDate, isSameMonthAndDay } from '@/utils';
 
 interface Props {
   onClick: () => void;
@@ -13,8 +14,13 @@ interface Props {
 const BirthdayContent: React.FC<Props> = ({ birthday, name, onClick }) => (
   <Container onClick={onClick}>
     <FrameContainer>
+      <BirthdayText>{formatDate(birthday)}</BirthdayText>
       <BirthdayFrame />
     </FrameContainer>
+    <CongText>
+      {isSameMonthAndDay(birthday) ? '오늘은' : formatDate(birthday)} {name}님의
+      생일이에요!!
+    </CongText>
   </Container>
 );
 
@@ -35,4 +41,22 @@ const Container = styled.div`
 const FrameContainer = styled.div`
   position: absolute;
   top: 1.1875rem;
+`;
+
+const Text = styled.p`
+  color: #000;
+  font-weight: 600;
+`;
+
+const BirthdayText = styled(Text)`
+  font-size: 1.125rem;
+  position: absolute;
+  top: 0.75rem;
+  left: 0.75rem;
+`;
+
+const CongText = styled(Text)`
+  font-size: 1rem;
+  margin-top: 8.1875rem;
+  z-index: 3;
 `;
