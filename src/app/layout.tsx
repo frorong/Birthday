805519709fe0987ac64f9c5155cd64336f0bb25c
frorong <moondgod@gmail.com>
2024-12-11 16:07:35 +0900

    readme 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,25 +15,38 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "산타가 연말 선물을 준비했어요!",
   description: "지금 바로 산타가 보낸 선물 확인하기",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["토스", "선물", "산타", "크리스마스"],
+  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#000000" }],
+  metadataBase: new URL("https://toss-im-mn12ads1gdd8fd6f.vercel.app"),
+  viewport:
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  applicationName: "토스 산타 선물",
   openGraph: {
     title: "산타가 연말 선물을 준비했어요!",
     description: "지금 바로 산타가 보낸 선물 확인하기",
-    type: "website",
+    url: "https://toss-im-mn12ads1gdd8fd6f.vercel.app/",
+    siteName: "toss.im",
     images: [
       {
-        url: "https://toss-im-mn12ads1gdd8fd6f.vercel.app/open-graph.png",
+        url: "/open-graph.png",
         width: 1200,
         height: 630,
         alt: "산타의 선물",
       },
     ],
-    siteName: "산타의 선물",
+    locale: "ko",
+    type: "website",
   },
   twitter: {
     card: "summary_large_image",
     title: "산타가 연말 선물을 준비했어요!",
     description: "지금 바로 산타가 보낸 선물 확인하기",
-    images: ["https://toss-im-mn12ads1gdd8fd6f.vercel.app/open-graph.png"],
+    images: ["/open-graph.png"],
+  },
+  icons: {
+    icon: "/favicon.ico",
   },
 };
 
@@ -44,35 +57,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <head>
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:url"
-          content="https://toss-im-mn12ads1gdd8fd6f.vercel.app"
-        />
-        <meta property="og:title" content="산타가 연말 선물을 준비했어요!" />
-        <meta
-          property="og:description"
-          content="지금 바로 산타가 보낸 선물 확인하기"
-        />
-        <meta
-          property="og:image"
-          content="https://toss-im-mn12ads1gdd8fd6f.vercel.app/open-graph.png"
-        />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="산타가 연말 선물을 준비했어요!" />
-        <meta
-          name="twitter:description"
-          content="지금 바로 산타가 보낸 선물 확인하기"
-        />
-        <meta
-          name="twitter:image"
-          content="https://toss-im-mn12ads1gdd8fd6f.vercel.app/open-graph.png"
-        />
-      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
